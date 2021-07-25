c52bc6250f381f9503b58c626bbd6645bb40ec0a
k-yamaki <k.yamaki@hiroshimamm.com>
2021-07-25 19:01:59 +0900

    Initial Commit

---
+++ A1_SwiftUI_WebAPI/A1_SwiftUI_WebAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  A1_SwiftUI_WebAPIApp.swift
+//  A1_SwiftUI_WebAPI
+//
+//  Created by keiji yamaki on 2021/07/25.
+//
+
+import SwiftUI
+
+@main
+struct A1_SwiftUI_WebAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A1_SwiftUI_WebAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  A1_SwiftUI_WebAPI
+//
+//  Created by keiji yamaki on 2021/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
